ac92e7f8df99f5353927f09f6760271378e5e644
Integral <integral@clarify.local>
2025-02-17 17:14:11 -0300

    finalizacao primeira aula

--- 02_pandas/readCsv.py
+++ 02_pandas/readCsv.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import datetime as dt
-# import numpy as np
-# import matplotlib as plot
-# from matplotlib import pyplot
-# import seaborn as sborn
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import pyplot
+import seaborn as sborn
 
 from IPython.display import display
 
@@ -46,7 +46,7 @@ dfNy['year'] = dfNy['last_review'].dt.year
 # print(dfNy['last_review'])
 # print(dfNy.last_review)
 
-dfNy['price'] = dfNy['price'].mask(dfNy['year'] <= 2010, dfNy['price'] / 1.174)
+dfNy['price'] = dfNy['price'].mask(dfNy['year'] <= 2011, dfNy['price'] / 1.174)
 dfNy['price'] = dfNy['price'].mask(dfNy['year'] <= 2012, dfNy['price'] / 2.174)
 dfNy['price'] = dfNy['price'].mask(dfNy['year'] <= 2013, dfNy['price'] / 3.174)
 dfNy['price'] = dfNy['price'].mask(dfNy['year'] <= 2014, dfNy['price'] / 4.174)
@@ -85,8 +85,11 @@ for i in variaveis:
     dado.append(dfRj[i].isnull().sum()/dfRj[i].shape[0])
     vz.append(dado[:])
     dado.clear()
+print(vz)
 
 pd.DataFrame(vz, columns=['New York', 'Rio de Janeiro'], index=variaveis)
+print(pd.DataFrame(vz, columns=['New York', 'Rio de Janeiro'], index=variaveis))
+
 dfNy_clean = dfNy.dropna(subset=['name', 'host_name'], axis=0)   
 dfRj_clean = dfRj.dropna(subset=['name', 'host_name'], axis=0)
 
@@ -105,3 +108,47 @@ lr_rj_mediana = dfRj_clean['last_review'].astype('datetime64[ns]').quantile(0.5,
 #substituir pelo valor calculado do last_review pela interpolação midpoint
 dfNy_clean = dfNy_clean.fillna({'last_review': lr_ny_mediana}) 
 dfRj_clean = dfRj_clean.fillna({'last_review': lr_rj_mediana})
+
+dx0 = ['price', 'minimum_nights']
+
+# for aux in dx0:
+#     data_A = dfNy_clean[aux]
+#     data_B = dfRj_clean[aux]
+#     data_2d = [data_A, data_B]
+
+#     plt.boxplot(data_2d, vert=False, labels=["New York", "Rio de Janeiro"])
+#     plt.title(aux)
+#     plt.show()
+
+dfNy_clean[['price', 'minimum_nights','number_of_reviews',
+                'reviews_per_month',
+                'calculated_host_listings_count','availability_365']].describe
+    
+dfNy_copy = dfNy_clean.copy()
+dfNy_copy.drop(dfNy_copy[dfNy_copy.price > 1100].index, axis=0, inplace=True)
+dfNy_copy.drop(dfNy_copy[dfNy_copy.minimum_nights > 66].index, axis=0, inplace=True)
+
+
+dfRj_clean[['price', 'minimum_nights','number_of_reviews',
+                'reviews_per_month',
+                'calculated_host_listings_count','availability_365']].describe
+
+dfrj_copy = dfRj_clean.copy()
+dfrj_copy.drop(dfrj_copy[dfrj_copy.price > 600].index, axis=0, inplace=True)
+dfrj_copy.drop(dfrj_copy[dfrj_copy.minimum_nights > 4].index, axis=0, inplace=True)
+
+var = ['Entire Home/Apt', 
+       'Private room',
+       'Shared room',
+       'Hotel room']
+
+dadoVar = {}
+
+for aux in var:
+    dadoVar[aux] = [dfNy_copy.loc[dfNy_copy.room_type == aux].shape[0]/dfNy_copy.room_type.shape[0],    
+                    dfrj_copy.loc[dfrj_copy.room_type == aux].shape[0]/dfrj_copy.room_type.shape[0] ]
+ima = pd.DataFrame(dadoVar, index=['New York', 'Rio de Janeiro'])
+ima.plot(kind="barh", stacked=True, figsize=(6,4), color=['blue', 'red', 'green', 'yellow'])
+plt.legend(loc="lower left", bbox_to_anchor=(0.8, 1.0))
+plt.title("Room Type")
+plt.show()
