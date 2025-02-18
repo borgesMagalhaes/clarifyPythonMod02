c4307126d747e96c8c3135ff1de2300c1c103a83
Bruno B. Magalhães <borges.magalhaes@gmail.com>
2025-02-18 17:15:12 -0300

    finaliação do app.callback

--- 04_main_store/main_store.py
+++ 04_main_store/main_store.py
@@ -1,3 +1,4 @@
+import os
 import plotly.graph_objects as graph_obj
 import plotly.express as px
 import pandas as pd
@@ -15,6 +16,9 @@ url_vapor_theme = dbc.themes.VAPOR
 
 #importando dados do csv dataset_comp.csv
 df = pd.read_csv('dataset_comp.csv')
+
+# file_path = os.path.join(os.getcwd(), 'dataset_comp.csv')
+# df = pd.read_csv(file_path)
 
 #Transformando a coluna dt_Venda em datetime
 df['dt_Venda']=pd.to_datetime(df['dt_Venda'])
@@ -132,14 +136,80 @@ layout_linha01 = html.Div([
     'margin-top': '40px',
     'height': '300px'})
 layout_linha02 = html.Div([  
-    html.Div([  ]) ,
-    html.Div([  ])
+    html.Div([  
+        html.H4('Vendas por Mês e Loja/Cidade'),
+        dcc.Graph(id='visual02')        
+    ], style={'width': '60%',
+              'text-align': 'center'}),
+    html.Div(
+        dcc.Graph(id='visual03')
+    , style={'width': '35%',
+              'text-align': 'center'})
+],style={
+    'display': 'flex',
+    'justify-content': 'space-around',
+    'margin-top': '40px',
+    'height': '150px'})
+
+####################CARREGAR O LAYOUT###########################
+app.layout = html.Div([
+    layout_titulo, 
+    layout_linha01, 
+    layout_linha02
 ])
 ####################FUNCOES DE APOIO###########################
+def filtro_cliente(cliente_selecionado):
+    if cliente_selecionado is None:
+        return pd.Series(True, index=df.index)
+    else:
+        return df['Cliente'] == cliente_selecionado
+    
+def filtro_categoria(categoria_selecionada):
+    if categoria_selecionada is None:
+        return pd.Series(True, index=df.index)
+    elif categoria_selecionada == 'todas_categorias':
+        return pd.Series(True, index=df.index)
+    return df['Categorias'] == categoria_selecionada
 
+def filtro_mes(meses_selecionados):
+    if meses_selecionados is None:
+        return pd.Series(True, index=df.index)
+    elif 'ano_completo' in meses_selecionados:
+        return pd.Series(True, index=df.index)
+    else:
+        return df['Mes'].isin(meses_selecionados)
 ####################CALLBACKS##################################
+@app.callback(
+    Output('output_cliente', 'children'),
+    [
+       Input('drpCliente', 'value'),
+       Input('rdCategoria', 'value') 
+    ]
+)
 
+def atualizar_texto(cliente_selecionado, categoria_selecionada):
+    if cliente_selecionado and categoria_selecionada:
+        return f'TOP5 {categoria_selecionada} | Cliente: {cliente_selecionado}'
+    
+    elif cliente_selecionado:
+        return f'TOP5 Produtos | Cliente: {cliente_selecionado}'
+    
+    elif categoria_selecionada:
+        return f'TOP5 {categoria_selecionada}'
+    
+    return f'TOP5 Categorias'
 
+#Callback para atualizar o tema do Dash App com o Dash Bootstrap Components e o Dash Bootstrap Templates 
+@app.callback(
+    Output('visual01', 'figure'),
+    [
+        Input('drpCliente', 'value'),
+        Input('chkMes', 'value'),
+        Input('rdCategoria', 'value'),
+        Input(ThemeSwitchAIO.ids.switch('theme'), 'value')
+    ]
+)
+        
 #executa o server e o app se o arquivo for executado diretamente
 if __name__ == '__main__':
     app.run_server(debug=True)
